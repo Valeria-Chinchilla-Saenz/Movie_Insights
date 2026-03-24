fd911cef5092e69a595aa4c9e5ebac7d522b28d7
stephanieRC08 <stephanierc2006@gmail.com>
2026-03-24 12:44:13 -0600

    Clase visualizador actualizada con los Insight

--- scr/visualizacion/ClaseVisualizador.py
+++ scr/visualizacion/ClaseVisualizador.py
@@ -22,6 +22,11 @@ class Visualizador:
         plt.xlabel('Promedio de Votos ')
         plt.ylabel('Cantidad de Películas')
         plt.title('Catidad de peliculas y su distribucion de promedio de Votos')
+        ax.text(0.1, 0.9,
+                "Insight: La distribución es normal pero tiene un pico cerca del 6.2.\n"
+                    "y esto lo que indica es que el catalogo tiene calidad balanceada,\n",
+                transform=ax.transAxes, fontsize=6, verticalalignment='top',
+                bbox=dict(boxstyle='round', facecolor='white', alpha=0.5))
         return fig
 
 # scatter,
@@ -29,8 +34,15 @@ class Visualizador:
 
         filtro_ceros = self.df[(self.df['vote_average'] > 0) & (self.df['popularity'] > 0)].copy()
 
+        tituloyInsight = (
+            "<b>Relacion entre la Popularidad de las Peliculas y su Promedio de Votos</b><br>"
+            
+            "<i>¿Catidad de votos es igual a Popularidad?</i><br>"
+            "<i>Notamos que las notas altas (8+) no garantizan la mayor popularidad masiva.</i>"
+        )
+
         fig = px.scatter(filtro_ceros,x= 'vote_average',y='popularity',hover_name="title",color="vote_count",
-                         title="Relacion entre la Popularidad de las Peliculas y su Promedio de Votos",labels={'vote_average': 'Calificación', 'popularity': 'Popularidad'})
+                         title=tituloyInsight,labels={'vote_average': 'Calificación', 'popularity': 'Popularidad'})
 
         return fig
 
@@ -62,6 +74,12 @@ class Visualizador:
         ax.set_yticklabels(columnas, fontsize=10)
 
         ax.set_title('Mapa de Correlaciones de Movie Insights', pad=30)
+        ax.text(0.5, -0.25,
+                "Insight: La correlacion mas relevante es entre 'vote_average' y 'vote_count' con un 0.37\n"
+                "Esto sugiere que a mayor calidad(vote_average) percibida, mayor es el volumen de interaccion('vote_count').\n"
+                "La popularidad muestra una independencia casi total de la calificación promedio de calidad(vote_average) con un 0.01",
+                transform=ax.transAxes, fontsize=10, ha='center',
+                bbox=dict(boxstyle='round', facecolor='azure', alpha=0.8))
 
         # no corta nada la mostrar
         plt.tight_layout()
@@ -72,7 +90,7 @@ class Visualizador:
 #barras verticales
     def barras_estadis(self, resumen_est, nom_columna):
 
-        nombres= ['Mínimo', 'Promedio', 'Máximo']
+        nombres= ['Minimo', 'Promedio', 'Maximo']
         valores = [resumen_est.loc['min',nom_columna], resumen_est.loc['mean',nom_columna], resumen_est.loc['max',nom_columna]]
 
         fig, ax = plt.subplots(figsize=(10, 6))
@@ -80,10 +98,17 @@ class Visualizador:
         barra= plt.bar(nombres,valores, color='skyblue')
         plt.bar_label(barra, padding=3, fmt='%.2f')
 
-        plt.xlabel('Estadísticas')
+        plt.xlabel('Estadisticas')
         plt.ylabel(f'Valores de {nom_columna}')
-        plt.title(f'Análisis de {nom_columna}: Mínimo, Promedio y Máximo')
+        plt.title(f'Analisis de {nom_columna}: Minimo, Promedio y Maximo')
 
+        diferencia = resumen_est.loc['max', nom_columna] / resumen_est.loc['mean', nom_columna]
+        ax.text(0.5, 0.95,
+                f"Insight: Existe una alta disparidad en {nom_columna}.\n"
+                f"El valor máximo es aproximadamente {diferencia:.1f} veces mayor que el promedio,\n"
+                "lo que confirma la presencia de 'Outliers' o exitos excepcionales en los datos.",
+                transform=ax.transAxes, fontsize=9, ha='center', va='top',
+                bbox=dict(boxstyle='round', facecolor='white', alpha=0.8))
         return fig
 
 #barras
@@ -98,12 +123,22 @@ class Visualizador:
 
         ax.set_title('Distribucion del catalogo de las paliculas por Idioma Original')
         ax.set_xlabel('Idioma Original')
-        ax.set_ylabel('Cantidad de Películas')
+        ax.set_ylabel('Cantidad de Peliculas')
 
         plt.xticks(rotation=45)
 
         for i, v in enumerate(cantidades):
-            ax.text(i, v + 5, str(v), ha='center', va='bottom', fontsize=9)
+            ax.text(i, v + 5, str(v), ha='center', va='bottom', fontsize=8)
+
+
+        ax.text(0.95, 0.85,
+                "Insight: El catalogo muestra que el inglés es el mas comun en los datos\n"
+                "tiene mas de 5,500 títulos, lo que representa una centralizacion\n"
+                "en la industria de Hollywood frente a otros mercados globales.",
+                transform=ax.transAxes, fontsize=10, ha='right', va='top',
+                bbox=dict(boxstyle='round', facecolor='white', alpha=0.7))
+
+
 
         plt.tight_layout()
         return fig
@@ -114,9 +149,13 @@ class Visualizador:
         #Ordenar el DF por vote_count y tomar las top 10
         top_10 = self.df.nlargest(10, 'vote_count')
 
+        tituloyInsight = (
+            "<b>Top 10 Titulos con Mayor Cantidad de Votos</b><br>"
 
+            "<i>Se puede ver que Spider-Man lidera con una diferencia masiva sobre el resto del catálogo.</i><br>"
+        )
         fig = px.bar(top_10, x='vote_count', y='title', orientation='h', color='vote_count',
-                     title='Top 10 Titulos con Mayor Cantidad de Votos',text="vote_count", color_continuous_scale='Blues' )
+                     title=tituloyInsight,text="vote_count", color_continuous_scale='Blues' )
 
 
         fig.update_layout(yaxis={'categoryorder': 'total ascending'})  # Ordena de mayor a menor
@@ -149,6 +188,13 @@ class Visualizador:
 
         # Cuadricula para facilitar la lectura
         ax.grid(True, linestyle='--', alpha=0.7)
+
+        ax.text(0.02, 0.95,
+                "Insight: Se observa un crecimiento a partir de 2024\n"
+                "Esto sugiere que pudo haber ocurrido un aumento real \n"
+                " en el consumo de cine post-pandemia o se digitalizaron mas obras en este periodo.",
+                transform=ax.transAxes, fontsize=10, verticalalignment='top',
+                bbox=dict(boxstyle='round', facecolor='white', alpha=0.8))
 
         plt.tight_layout()
         return fig
@@ -185,6 +231,13 @@ class Visualizador:
         for i, barra in enumerate(barras):
             width = barra.get_width()
             ax.text(width + 5, i, f'{width:.1f}', va='center', fontweight='bold')
+
+        ax.text(0.98, 0.2,
+                "Insight: 'Frankenstein' (2025) rompe la tendencia historica\n"
+                "con 951.3 puntos de popularidad, superando por mas de\n"
+                "5 veces el exito del año anterior ('Abyss', 179.6).",
+                transform=ax.transAxes, fontsize=10, ha='right',
+                bbox=dict(boxstyle='round', facecolor='azure', alpha=0.8))
 
         plt.tight_layout()
         return fig
